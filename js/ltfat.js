2be5fa5ea2cd831d4bbd03f665f1c95147a7bdcf
Zdenek Prusa <zdenek.prusa@gmail.com>
2015-03-13 10:56:58 +0100

    Starting version
    Dynamical inclusion of other html files works.

--- js/ltfat.js
+++ js/ltfat.js
@@ -1,39 +1,56 @@
 var baseurl = "."
-
 $(document).ready(function() {
     includefile();
-    $('body').show();
 });
 
+function includefile(){
+    var inLen = $('.include[file]').length;
+    $('.include[file]').each(function (idx, el) {
+        var included_file = $(this).attr('file');
+        //console.log(included_file);
 
-function relativetoabsolute(){
+        var wasinhead = $("head").html();
+        $(this).load(included_file,function (){
+
+            if($(this).is("head"))
+        {
+            // console.log('This was head');
+            $(this).html(function(i,origText){
+                return  wasinhead + origText;
+            });
+            $(this).removeAttr('class');
+        }
+
+
+        $(this).find("img").attr('src',function(index,currVal){
+            return relativetoabsolute(currVal);
+        });
+
+        $(this).find("a").attr('href',function(index,currVal){
+            return relativetoabsolute(currVal);
+        });
+        // This has to be inside load because it is called asynchronously
+        // And we want to run it only after the last item was loaded
+        if(idx==inLen-1)
+        {  
+            $('body').css({display:'block'});
+        }
+        });
+        // Remove the include class and file attribute to avoid including it again
+        $(this).removeAttr('file');
+        $(this).removeClass('include');
+    });
 }
 
-function includefile(){
-    $('.include').each(function () {
-    var included_file = $(this).attr('file');
-    console.log(included_file);
-    if (typeof included_file != "undefined")
+function relativetoabsolute(v){
+    var n = v.lastIndexOf('#BASEURL#');
+    if( n!= -1)
     {
-        $(this).load(included_file,function (){
-            $(this).find("img").attr('src',function()
-                {
-                    var n =this.src.lastIndexOf('#BASEURL#');
-                    if( n!= -1)
-                    {
-                        var valid = this.src.substring(n);
-                        var retval = valid.replace(/^#BASEURL#/,baseurl);
+        var valid = v.substring(n);
+        var retval = valid.replace(/^#BASEURL#/,baseurl);
 
-                        console.log('Old:' + this.src);
-                        console.log('New:' + retval);
-                        return retval;
-                    }
-                });
-        });
+        //console.log('Old:' + v);
+        //console.log('New:' + retval);
+        return retval;
     }
-    else
-    {
-        console.error("#include is missing file");
-    }
-    });
 }
