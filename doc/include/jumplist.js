0253d12975cf5f87b98c6104769b786dce0c5f01
Zdenek Prusa <zdenek.prusa@gmail.com>
2015-04-30 15:55:39 +0200

    The jumplist in documentation works correctly now

--- doc/include/jumplist.js
+++ doc/include/jumplist.js
@@ -1,31 +1,47 @@
 $(document).ready(function(){
-// 
-// globalfun list and toolboxversion variables are defined
-if(typeof globalfunlist === 'undefined'){
-$('#jumplist').html('No function list found');
-}
-else{
+    // 
+    // globalfun list and toolboxversion variables are defined
+    if(typeof globalfunlist === 'undefined'){
+        $('#jumplist').html('No function list found');
+    }
+    else{
+        // Build a combobox with function names
+        var jstr = '';
+        jstr += '<div class="input-group pull-right">';
+        jstr += '<span class="input-group-addon"><b>Go to function</b></span>';
+        jstr += '<select class="form-control input-sm">';
 
-// Build a combobox with function names
-var jstr = '';
-jstr += '<div class="input-group pull-right">';
-jstr += '<span class="input-group-addon"><b>Go to function</b></span>';
-jstr += '<select class="form-control input-sm">';
+        jstr += '<option value="start/index">---Home---</option>'; 
+        for(var fname in globalfunlist){
+            var relpath = globalfunlist[fname];
+            jstr += '<option value="'+relpath+'">'+fname+'</option>';
+        }
+        jstr += '</select>';
+        jstr += '</div>';
+        $('#jumplist').html(jstr);
 
-for(var fname in globalfunlist){
-    var relpath = globalfunlist[fname];
-    jstr += '<option value="'+relpath+'">'+fname+'</option>';
-}
-jstr += '</select>';
-jstr += '</div>';
-$('#jumplist').html(jstr);
+        var baseurl = loc.protocol + "//" + loc.host + '/doc';
+        var valuestr = $(location).attr("href").substr(baseurl.length+1);
+        if(valuestr.indexOf('.html') > -1){
+            if(valuestr.indexOf('_code.html') > -1){
+                valuestr = valuestr.substr(0,valuestr.indexOf('_code.html'));
+            }
+            else{
+                valuestr = valuestr.substr(0,valuestr.indexOf('.html'));
+            }
+            //$('#jumplist option[value="'+valuestr.substr(valuestr.length-4)+'"]').attr('selected','selected');
+            $('#jumplist option[value="'+valuestr+'"]').attr('selected','selected');
+        }
+
+        // Redirect browser to new location on combobox change
+        $('#jumplist select').change(function(){
+            $('#jumplist option[selected]').removeAttr('selected');
+            window.location.replace('../' + this.value + '.html');
+        });
 
 
-// Redirect browser to new location on combobox change
-$('#jumplist select').change(function(){
-    window.location.replace('../' + this.value + '.html');
-});
-}
+
+    }
 
 // Add classes to the code-doc switch
 $('#codeswitch a').addClass('btn btn-info btn-block');
@@ -33,7 +49,7 @@ $('#codeswitch a').addClass('btn btn-info btn-block');
 var menutitle = $('#seealso #menutitle').html();
 $('#seealso #menutitle').html('<li><a href=""><b>'+menutitle +'</b></a></li>');
 $('#seealso ul').addClass('nav doc-sidenav affix-top')
-$('#seealso #menutitle > li').prependTo('#seealso ul');
+    $('#seealso #menutitle > li').prependTo('#seealso ul');
 
 
 });
