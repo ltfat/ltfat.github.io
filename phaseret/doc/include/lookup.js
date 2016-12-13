3d488d470df196852467a6f6a380df766b7bd2dd
Zdenek Prusa <zdenek.prusa@gmail.com>
2016-12-13 17:09:35 +0100

    Restructured documentation of phaseret

---
+++ phaseret/doc/include/lookup.js
@@ -0,0 +1,22 @@
+var globalfunlist = {
+   decolbfgs:'gabor/decolbfgs',
+   demo_blockproc_phaseret:'demos/demo_blockproc_phaseret',
+   demo_blockproc_phaseret2:'demos/demo_blockproc_phaseret2',
+   demo_blockproc_phaseretmix:'demos/demo_blockproc_phaseretmix',
+   demo_vocoderphaseret:'demos/demo_vocoderphaseret',
+   demo_windowaliasing:'demos/demo_windowaliasing',
+   gla:'gabor/gla',
+   gsrtisila:'gabor/gsrtisila',
+   legla:'gabor/legla',
+   lertisila:'gabor/lertisila',
+   magnitudeerr:'gabor/magnitudeerr',
+   magnitudeerrdb:'gabor/magnitudeerrdb',
+   pghi:'gabor/pghi',
+   pghi_findgamma:'gabor/pghi_findgamma',
+   plotdgtrealphasediff:'gabor/plotdgtrealphasediff',
+   rtisila:'gabor/rtisila',
+   rtpghi:'gabor/rtpghi',
+   spsi:'gabor/spsi',
+};
+
+ var toolboxversion='0.2.0'; 
